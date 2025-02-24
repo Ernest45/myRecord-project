5cb03dea8cdf50e581cf90bb60a4c5e36dd03fb1
Ernest45 <gkgkswns71@gmail.com>
2025-02-24 21:06:54 +0900

    Jpa 메서드 사용 확인

---
+++ src/main/java/me/hanjun/springbootblog/memberService.java
@@ -0,0 +1,31 @@
+package me.hanjun.springbootblog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class memberService {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+
+    public void test() {
+
+        //생성
+        memberRepository.save(new Member(1L, "a"));
+
+        //조회
+
+        Optional<Member> member = memberRepository.findById(1L); // 단건 조회
+        List<Member> allMember = memberRepository.findAll();
+
+        //삭제
+
+        memberRepository.deleteById(1L);
+
+    }
+}
