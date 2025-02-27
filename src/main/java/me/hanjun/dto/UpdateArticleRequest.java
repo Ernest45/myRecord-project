3e359964762687ab74b3aff5a57804277322f728
Ernest45 <gkgkswns71@gmail.com>
2025-02-27 21:32:32 +0900

    글삭제 기능 추가

---
+++ src/main/java/me/hanjun/dto/UpdateArticleRequest.java
@@ -0,0 +1,2 @@
+package me.hanjun.dto;public class UpdateArticleRequest {
+}
--- src/test/java/me/hanjun/controller/BlogApiControllerTest.java
+++ src/test/java/me/hanjun/controller/BlogApiControllerTest.java
@@ -19,6 +19,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -147,7 +148,31 @@ class BlogApiControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content").value(content))
                 .andExpect(jsonPath("$.title").value(title));
-        // jsonpath의 사용법
+        // jsonpath의 사용
+    }
+
+    @DisplayName("deleteArticle : 블로그 글 삭제에 성공한다")
+    @Test
+    public void deleteArticle() throws Exception {
+        //given
+        final String url = "/api/articles/{id}";
+        final String title = "title";
+        final String content = "content";
+
+        Article savedArticle = blogRepository.save(Article.builder()
+                .title(title)
+                .content(content)
+                .build());
+
+        //when
+        mockMvc.perform(delete(url, savedArticle.getId()))
+                .andExpect(status().isOk());
+
+        //then
+
+        List<Article> articles = blogRepository.findAll();
+
+        assertThat(articles).isEmpty();
     }
 
 }
