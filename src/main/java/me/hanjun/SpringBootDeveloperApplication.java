dc2c39596b0927f7f73fec58f9747e08c12201a6
Ernest45 <gkgkswns71@gmail.com>
2025-03-02 01:25:42 +0900

    블로그를 jsp thymeleaf를 통해 구현(get Id)로 게시물 하나 찾기

--- src/main/java/me/hanjun/SpringBootDeveloperApplication.java
+++ src/main/java/me/hanjun/SpringBootDeveloperApplication.java
@@ -2,8 +2,10 @@ package me.hanjun;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
--- src/main/java/me/hanjun/controller/BlogViewController.java
+++ src/main/java/me/hanjun/controller/BlogViewController.java
@@ -1,12 +1,16 @@
 package me.hanjun.controller;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import me.hanjun.domain.Article;
 import me.hanjun.dto.ArticleListViewResponse;
 import me.hanjun.dto.ArticleResponse;
+import me.hanjun.dto.ArticleViewResponse;
 import me.hanjun.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -28,5 +32,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
