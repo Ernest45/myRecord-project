b7f9b4f0f5f8a9935260cb52cd9524d79424eb1b
Ernest45 <gkgkswns71@gmail.com>
2025-03-28 21:47:29 +0900

    TokenApiControllerTest 생성

---
+++ src/test/java/me/hanjun/controller/TokenApiControllerTest.java
@@ -0,0 +1,25 @@
+package me.hanjun.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class TokenApiControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+
+    @Test
+    void createNewAccessToken() {
+    }
+}
