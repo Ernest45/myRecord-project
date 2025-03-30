7b0aab4627fefec1a919f74f641897b78c61ecc9
Ernest45 <gkgkswns71@gmail.com>
2025-03-30 18:24:49 +0900

    Remove .env from Git tracking

--- src/main/java/me/hanjun/SpringBootDeveloperApplication.java
+++ src/main/java/me/hanjun/SpringBootDeveloperApplication.java
@@ -1,5 +1,6 @@
 package me.hanjun;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +8,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class SpringBootDeveloperApplication {
+
+    static {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
