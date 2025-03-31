64ccf471d59d0c1c2e2dc1cb1cdd50af7173c186
Ernest45 <gkgkswns71@gmail.com>
2025-04-01 02:20:35 +0900

    @repository 애너테이션 추가

--- src/main/java/me/hanjun/repository/BlogRepository.java
+++ src/main/java/me/hanjun/repository/BlogRepository.java
@@ -2,6 +2,8 @@ package me.hanjun.repository;
 
 import me.hanjun.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends JpaRepository<Article, Long> {
 }
