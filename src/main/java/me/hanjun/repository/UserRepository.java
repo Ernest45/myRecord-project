e6a2b2ab4c8e0e37c19d8ea5138667129b1e0563
Ernest45 <gkgkswns71@gmail.com>
2025-04-01 02:20:45 +0900

    @repository 애너테이션 추가

--- src/main/java/me/hanjun/repository/UserRepository.java
+++ src/main/java/me/hanjun/repository/UserRepository.java
@@ -2,9 +2,10 @@ package me.hanjun.repository;
 
 import me.hanjun.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
 }
