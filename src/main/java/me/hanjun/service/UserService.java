48ac59136abf07658e6de32e57c82167aa778bd0
Ernest45 <gkgkswns71@gmail.com>
2025-04-01 01:22:41 +0900

    userSevice findByEmail 메서드추가

--- src/main/java/me/hanjun/service/UserService.java
+++ src/main/java/me/hanjun/service/UserService.java
@@ -28,4 +28,10 @@ public class UserService {
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 
+    public User findByEmail(String email) {
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("unexpected user"));
+    }
+
 }
