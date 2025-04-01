5a8728f65bd8617faf13d3ebed07e912aae9e15f
Ernest45 <gkgkswns71@gmail.com>
2025-04-01 21:28:52 +0900

    userService 및 controller 에서 oauth

--- src/main/java/me/hanjun/service/UserService.java
+++ src/main/java/me/hanjun/service/UserService.java
@@ -12,12 +12,15 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
 
     public Long save(AddUserRequest dto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(encoder.encode(dto.getPassword()))
                 // config에서 등록한 암호화 등록
                 .build()).getId();
     }
