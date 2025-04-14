79b5d3f45880cf78b45317fc914e05247a199b71
Ernest45 <gkgkswns71@gmail.com>
2025-04-15 03:26:33 +0900

    카카오 오어스

--- src/main/java/me/hanjun/controller/OauthController.java
+++ src/main/java/me/hanjun/controller/OauthController.java
@@ -1,28 +1,28 @@
-package me.hanjun.controller;
-
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequiredArgsConstructor
-public class OauthController {
-    private final OauthService oauthService;
-    
-    @GetMapping("/login/oauth2/code/{provider}")
-    public ResponseEntity<?> oauthLogin(
-            @PathVariable("provider") String provider,
-            @RequestParam("code") String code) {
-        
-        try {
-            OauthResponse response = oauthService.handleOauthLogin(provider, code);
-            return ResponseEntity.ok(response);
-        } catch (IllegalArgumentException e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
-    }
-}
+//package me.hanjun.controller;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequiredArgsConstructor
+//public class OauthController {
+//    private final OauthService oauthService;
+//
+//    @GetMapping("/login/oauth2/code/{provider}")
+//    public ResponseEntity<?> oauthLogin(
+//            @PathVariable("provider") String provider,
+//            @RequestParam("code") String code) {
+//
+//        try {
+//            OauthResponse response = oauthService.handleOauthLogin(provider, code);
+//            return ResponseEntity.ok(response);
+//        } catch (IllegalArgumentException e) {
+//            return ResponseEntity.badRequest().body(e.getMessage());
+//        }
+//    }
+//}
--- src/main/java/me/hanjun/util/kakao/KakaoUtil.java
+++ src/main/java/me/hanjun/util/kakao/KakaoUtil.java
@@ -1,16 +1,16 @@
-package me.hanjun.util.kakao;
-
-import org.springframework.beans.factory.annotation.Value;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-
-@Component
-@Slf4j
-public class KakaoUtil {
-    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String clientId;
-    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
-    private String redirectUri;
-
-
-}
+//package me.hanjun.util.kakao;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Slf4j
+//public class KakaoUtil {
+//    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+//    private String clientId;
+//    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+//    private String redirectUri;
+//
+//
+//}
