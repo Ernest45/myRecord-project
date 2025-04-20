6a529b0137b07e5ce74a740bcf6173cbc6007b64
Ernest45 <gkgkswns71@gmail.com>
2025-04-21 07:43:31 +0900

    여러 플랫폼을 위한  infoFactory

--- src/main/java/me/hanjun/config/oauth/provider/OAuth2UserInfoFactory.java
+++ src/main/java/me/hanjun/config/oauth/provider/OAuth2UserInfoFactory.java
@@ -1,4 +1,15 @@
 package me.hanjun.config.oauth.provider;
 
+import java.util.Map;
+
 public class OAuth2UserInfoFactory {
+
+    public static OAuth2UserInfo create(String registrationId,
+                                        String accessToken,
+                                        Map<String, Object> attributes) {
+
+//        if (OAuth2Provider.KAKAO.getRegistrationId().equals(registrationId)) {
+            return new OAuth2KakaoUserInfo(accessToken, attributes);
+
+    }
 }
