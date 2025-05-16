4bb5cca29120563225b22d8627845c2b4908324d
Ernest45 <gkgkswns71@gmail.com>
2025-05-16 20:44:45 +0900

    redis 사용을 위한 RefReshToken redis 생성

---
+++ src/main/java/me/hanjun/domain/RefreshTokenRedis.java
@@ -0,0 +1,20 @@
+package me.hanjun.domain;
+
+
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@RedisHash(value = "refreshTokenRedis", timeToLive = 14440)
+public class RefreshTokenRedis {
+
+    @Id
+    private String refreshTokenRedis;
+    private Long userId;
+
+    public RefreshTokenRedis(String refreshTokenRedis, Long userId) {
+        this.refreshTokenRedis = refreshTokenRedis;
+        this.userId = userId;
+    }
+}
